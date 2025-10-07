83b056c31176fea2db0364ab841525746c9c9d44
amarpandey <amar.itdeveloper@gmail.com>
2025-10-08 00:45:13 +0530

    vahan response saved in db

--- routes/auth.js
+++ routes/auth.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const authController = require('../controllers/authController');
 const { registerUser } = require('../services/authService');
-require('dotenv').config();
+// require('dotenv').config();
 
 router.post('/login', authController.login);
 const bcrypt = require('bcryptjs');
