1543eda43fb6b0f2f4d647d6c97607d8c9ef62c7
amarpandey <amar.itdeveloper@gmail.com>
2026-01-25 01:53:43 +0530

    save driver data endpoint

--- services/saveDriveDataService.js
+++ services/saveDriveDataService.js
@@ -12,13 +12,22 @@ module.exports = (models) => {
         throw err;
       }
 
-      const record = await models.DiDriverData.create({
-        license_no,
-        client_id,
-        dob,
-        details
-      });
+      if (!license_no) {
+        const err = new Error('licenseNo is required');
+        err.status = 400;
+        throw err;
+      }
 
+      // Try to find existing record by license_no
+      let record = await models.DiDriverData.findOne({ where: { license_no } });
+
+      if (record) {
+        await record.update({ client_id, dob, details });
+        return record;
+      }
+
+      // Insert new
+      record = await models.DiDriverData.create({ license_no, client_id, dob, details });
       return record;
     }
   };
